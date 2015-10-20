473a207dd29a539aab7002afdca0c0433dc4555f
Ross14487 <Ross@CableMountain.net>
2015-10-19 19:11:00 -0600

    updated RegistrationSystem to include all methods

--- src/TrivaGameClient/RegistrationSystem.java
+++ src/TrivaGameClient/RegistrationSystem.java
@@ -1,6 +1,17 @@
 package TrivaGameClient;
 
+
+/******************************************************************************
+ * Name        :  Ross Mitchell 
+ * email       :  RPMitchell@mtech.edu
+ * Date        :  10-19-2012
+ * Description :  This is the RegistrationSystem Class for CSCI-466 Networks 
+ * Project 1, Trivia Game Server.  This class checks if the server is open,
+ * registers the player, deregister the player, and reports ready or not ready.
+ *      
+ ****************************************************************************/
 import java.net.UnknownHostException;
+import java.util.UUID;
 
 public class RegistrationSystem
 {
@@ -10,22 +21,20 @@ public class RegistrationSystem
     public RegistrationSystem(ServiceInterface service)
     {
        this.service = service;
-    }
+    }//RegistrationSystem
     
     private Message RegisterUser(String userName) throws UnknownHostException, IllegalArgumentException
     {
        service.sendMessage(new RegisterUserMessage(0x01, userName));
        return WaitForMessage();
-    }
-    
-    
-    
+    }//RegisterUser
+      
     private Message WaitForMessage()
     {
         Message msg = null;
         while ((msg = service.getQueuedMessage()) == null);
         return msg;
-    }
+    }//WaitForMessage
     
     public Message Registration (String userName) throws UnknownHostException, IllegalArgumentException
     {
@@ -37,16 +46,40 @@ public class RegistrationSystem
             return RegisterUser(userName);   
         }
         return msg;
-    }
+    }//Registration
     
     private boolean DeRegisterUser(UUID userID) throws UnknownHostException, IllegalArgumentException
     {
-        boolean success = false;
         Message msg = null;
         service.sendMessage(new UserIDMessage(0x02, userID));
         msg = WaitForMessage();
         if(!((StatusMessage)msg).getSuccess())
             errorMessage = ((StatusMessage)msg).getErrorMessage();
         return ((StatusMessage)msg).getSuccess();
-    }
-}
+    }//DeRegisterUser
+    
+    private boolean Ready (UUID userID) throws UnknownHostException, IllegalArgumentException
+    {
+        Message msg = null;
+        service.sendMessage(new UserIDMessage(0x03, userID));
+        msg = WaitForMessage();
+        if(!((StatusMessage)msg).getSuccess())
+            errorMessage = ((StatusMessage)msg).getErrorMessage();
+        return ((StatusMessage)msg).getSuccess();
+    }//Ready
+    
+    private boolean NotReady (UUID userID) throws UnknownHostException, IllegalArgumentException
+    {
+        Message msg = null;
+        service.sendMessage(new UserIDMessage(0x04, userID));
+        msg = WaitForMessage();
+        if(!((StatusMessage)msg).getSuccess())
+            errorMessage = ((StatusMessage)msg).getErrorMessage();
+        return ((StatusMessage)msg).getSuccess();
+    }//NotReady
+    
+    public String getErrorMessage()
+    {
+        return errorMessage;
+    }//getErrorMessage
+}//end RegistrationSystem class
